723b787e0a83fd4de70da7660d009601e7056d0a
PetrTaller <127184208+PetrTaller@users.noreply.github.com>
2024-02-21 17:49:00 +0100

    Add files via upload

---
+++ FUJKYDATABASEPROJEKT/FUJKYDATABASEPROJEKT/Program.cs
@@ -0,0 +1,16 @@
+﻿using FUJKYDATABASEPROJEKT;
+using System;
+
+public class Program
+    {
+        static void Main(string[] args)
+    {
+            ZakaznikDAO zakaznikDAO = new ZakaznikDAO();
+
+            Console.WriteLine("List of customers:");
+            foreach (var zakaznik in zakaznikDAO.GetAll())
+            {
+                Console.WriteLine(zakaznik);
+            }
+        }
+    }
