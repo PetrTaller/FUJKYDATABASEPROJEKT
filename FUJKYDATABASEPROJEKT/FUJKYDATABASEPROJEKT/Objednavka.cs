d237430d8b942b30017a0e277d4acc3ac6288ea9
PetrTaller <127184208+PetrTaller@users.noreply.github.com>
2024-02-23 12:38:49 +0100

    Add files via upload

---
+++ FUJKYDATABASEPROJEKT/FUJKYDATABASEPROJEKT/Objednavka.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FUJKYDATABASEPROJEKT
+{
+    public class Objednavka : IBaseClass
+    {
+        public int ID { get; set; }
+        public string CasDodani { get; set; }
+        public string NazevProduktu { get; set; }
+        public bool VipZakaznik { get; set; }
+        public int Zakaznik_ID { get; set; }
+
+        public Objednavka(int id, string casDodani, string nazevProduktu,bool vipZakaznik,int zakaznik_ID)
+        {
+            ID = id;
+            CasDodani = casDodani;
+            NazevProduktu = nazevProduktu;
+            VipZakaznik = vipZakaznik;
+            Zakaznik_ID = zakaznik_ID;
+        }
+        public override string ToString()
+        {
+            return ID + CasDodani + NazevProduktu + VipZakaznik + Zakaznik_ID;
+        }
+    }
+}
---
+++ FUJKYDATABASEPROJEKT/FUJKYDATABASEPROJEKT/Zakaznik.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FUJKYDATABASEPROJEKT
+{
+    public class Zakaznik : IBaseClass
+    {
+        public int ID { get; set; }
+        public string Jmeno { get; set; }
+        public string Email { get; set; }
+
+        public Zakaznik (int id, string jmeno, string email)
+        {
+            ID = id;
+            Jmeno = jmeno;
+            Email = email;
+        }
+        public override string ToString()
+        {
+            return ID+ Jmeno+ Email;
+        }
+    }    
+}
+
